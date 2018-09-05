4560f2c640c8d789cbcde55bd3960747ed47cfb0
yammik <mkim0514@gmail.com>
2018-09-04 22:25:46 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  def initialize(*args)
+    @l1 = args.sort[0]
+    @l2 = args.sort[1]
+    @l3 = args.sort[2]
+  end
+
+  def kind
+    is_triangle = (@l3 < @l1 + @l2) && (@l1 > 0) && (@l2 > 0) && (@l3 > 0)
+
+    if !is_triangle
+      raise TriangleError
+    else
+      if @l1 == @l2 && @l1 == @l3
+        :equilateral
+      elsif [@l1, @l2, @l3].uniq.length == 2
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+end
+
+class TriangleError < StandardError
 end
