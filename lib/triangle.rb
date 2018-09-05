b48d2b3962c98195ec13b83509d88dcc61ce39de
yapjinai <jinai.jyap@gmail.com>
2018-09-05 05:49:18 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,43 @@
+require 'pry'
 class Triangle
-  # write code here
+  def initialize(l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+
+  def kind
+
+    if !self.valid?
+      raise TriangleError
+
+    else
+      if
+       @l1 == @l2 && @l2 == @l3
+        :equilateral
+      elsif @l1 == @l2 || @l2 == @l3 || @l1 == @l3
+        :isosceles
+      else
+        :scalene
+      end
+    end
+
+  end
+
+  def valid?
+    @l1 > 0 &&
+    @l2 > 0 &&
+    @l3 > 0 &&
+    @l1 + @l2 > @l3 &&
+    @l2 + @l3 > @l1 &&
+    @l1 + @l3 > @l2
+  end
+
+  class TriangleError < StandardError
+    def message
+      "not a triangle!"
+    end
+  end
 end
+
+# binding.pry
